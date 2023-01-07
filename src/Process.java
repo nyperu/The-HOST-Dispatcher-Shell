0fceb1b214ac7236c25495be3c5f129374faa4f9
Taha-Adiguzel <102309946+Taha-Adiguzel@users.noreply.github.com>
2023-01-07 16:58:34 +0300

    Renklendirme İşlemleri yapıldı

--- src/Process.java
+++ src/Process.java
@@ -6,7 +6,7 @@ public class Process {
     public int olumAdimi;
     public int calistigiSure;
     public String Color;
-
+    
     public Process(int id, int varisZamani, int oncelik,int kalanZaman,String Color){
         this.id =id;
         this.varisZamani = varisZamani;
@@ -21,23 +21,23 @@ public class Process {
     }
     public void yeniBasladi(int saniye,int kalanSure){
 
-        System.out.println(saniye + ".0000 sn proses basladi      (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+")");
+        System.out.println(Color + saniye + ".0000 sn proses basladi      (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+")"+ "\u001B[0m");
         this.olumAdimi=0;
     }
     public void bitti(int saniye,int kalanSure){
 
-        System.out.println(saniye + ".0000 sn  proses bitti          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure);
+        System.out.println(Color +saniye + ".0000 sn  proses bitti          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ "\u001B[0m");
     }
     public void yurutuluyor(int saniye,int kalanSure){
         this.olumAdimi=0;
-        System.out.println(saniye + ".0000 sn  proses calisiyor          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure);
+        System.out.println(Color +saniye + ".0000 sn  proses calisiyor          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ "\u001B[0m");
     }
     public void zamanAsimi(int saniye,int kalanSure){
 
-        System.out.println(saniye + ".0000 sn  proses zaman asimi          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure);
+        System.out.println(Color +saniye + ".0000 sn  proses zaman asimi          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ "\u001B[0m");
     }
     public void askida(int saniye,int kalanSure){
         this.olumAdimi=0;
-        System.out.println(saniye + ".0000 sn  proses askida         (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure);
+        System.out.println(Color +saniye + ".0000 sn  proses askida         (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ "\u001B[0m");
     }
 }
