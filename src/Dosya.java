6fb1dbc938ba5d357275053bc4fe0cd738037053
Sezer Berber <nyperu14@gmail.com>
2023-01-07 18:46:06 +0300

    bitik haline çok yakın

--- src/Dosya.java
+++ src/Dosya.java
@@ -1,13 +1,13 @@
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import java.util.Random;
 public class Dosya {
     Process p;
     Node node;
 
     DoublyLinkedList list = new DoublyLinkedList();
 
-
+    Random rand = new Random();
     public DoublyLinkedList Oku(){
         String color="\u001B[3";
         int number = 0;
@@ -25,7 +25,7 @@ public class Dosya {
                 System.out.print("  Öncelik ; "+ saniye[1]);
                 System.out.println("  Süresi ; "+ saniye[2]);
                 */RenkCode(number);
-                p = new Process(id,Integer.parseInt(saniye[0]),Integer.parseInt(saniye[1]),Integer.parseInt(saniye[2]),NewColor(id));
+                p = new Process(id,Integer.parseInt(saniye[0]),Integer.parseInt(saniye[1]),Integer.parseInt(saniye[2]),NewColor(id),NewBColor(id));
                 node=new Node(p);
                 list.addLast(node);
                 id++;
@@ -41,18 +41,29 @@ public class Dosya {
         return null;
 
     }
-   /* public void listegetir(LinkedList p0){
+    /* public void listegetir(LinkedList p0){
 
-        for(int i = 0; !p0.IsEmpty();i++){
-            System.out.println( i +" sn " + p0.peek());
-            p0.pop();
-        }
-    }*/
+         for(int i = 0; !p0.IsEmpty();i++){
+             System.out.println( i +" sn " + p0.peek());
+             p0.pop();
+         }
+     }*/
     public String NewColor(int number){
-        return "\u001B[3" +RenkCode(number) +"m";
+        return "\u001B[1;3" +RenkCode(number) +"m";
     }
     public int RenkCode(int number){
         int newNumb = number %8;
+
         return newNumb;
+    }
+
+    public String NewBColor(int number){
+        int rd =rand.nextInt(8);
+        if(rd == number){
+            while(rd == number){
+                rd =rand.nextInt(8);
+            }
+        }
+        return "\u001B[10" +rd +"m";
     }
 }
--- src/Process.java
+++ src/Process.java
@@ -6,13 +6,16 @@ public class Process {
     public int olumAdimi;
     public int calistigiSure;
     public String Color;
-    
-    public Process(int id, int varisZamani, int oncelik,int kalanZaman,String Color){
-        this.id =id;
+
+    public String BackGround;
+
+    public Process(int id, int varisZamani, int oncelik,int kalanZaman,String Color,String BackGround){
+        this.id =0000+id;
         this.varisZamani = varisZamani;
         this.oncelik = oncelik;
         this.calisacagiSure = kalanZaman;
         this.Color = Color;
+        this.BackGround = BackGround;
         this.olumAdimi = varisZamani*-1;
         this.calistigiSure=0;
     }
@@ -20,24 +23,75 @@ public class Process {
         return this.id;
     }
     public void yeniBasladi(int saniye,int kalanSure){
+        if(saniye<10)
+            if(id<10)
+                 System.out.println(Color + BackGround+ saniye + ".0000 sn proses basladi         (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+" sn )"+ "\u001B[0m");
+            else
+                System.out.println(Color + BackGround+ saniye + ".0000 sn proses basladi        (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+" sn )"+ "\u001B[0m");
 
-        System.out.println(Color + saniye + ".0000 sn proses basladi      (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+")"+ "\u001B[0m");
+        if(saniye>=10)
+            if(id<10)
+                System.out.println(Color + BackGround+ saniye + ".0000 sn proses basladi        (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+" sn )"+ "\u001B[0m");
+            else
+                System.out.println(Color + BackGround+ saniye + ".0000 sn proses basladi       (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+" sn )"+ "\u001B[0m");
         this.olumAdimi=0;
     }
     public void bitti(int saniye,int kalanSure){
+        if(saniye<10)
+            if(id<10)
+                System.out.println(Color+ BackGround +saniye + ".0000 sn proses bitti           (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
+            else
+                System.out.println(Color+ BackGround +saniye + ".0000 sn proses bitti          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
 
-        System.out.println(Color +saniye + ".0000 sn  proses bitti          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ "\u001B[0m");
+        if(saniye>=10)
+            if(id<10)
+                System.out.println(Color+ BackGround +saniye + ".0000 sn proses bitti          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
+            else
+                System.out.println(Color+ BackGround +saniye + ".0000 sn proses bitti         (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
+
     }
     public void yurutuluyor(int saniye,int kalanSure){
         this.olumAdimi=0;
-        System.out.println(Color +saniye + ".0000 sn  proses calisiyor          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ "\u001B[0m");
+        if(saniye<10)
+            if(id<10)
+                System.out.println(Color+ BackGround +saniye + ".0000 sn proses yürütülüyor     (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
+            else
+                System.out.println(Color+ BackGround +saniye + ".0000 sn proses yürütülüyor    (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
+
+        if(saniye>=10)
+            if (id<10)
+                 System.out.println(Color+ BackGround +saniye + ".0000 sn proses yürütülüyor    (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
+            else
+                System.out.println(Color+ BackGround +saniye + ".0000 sn proses yürütülüyor   (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
+
     }
     public void zamanAsimi(int saniye,int kalanSure){
+        if(saniye<10)
+            if(id<10)
 
-        System.out.println(Color +saniye + ".0000 sn  proses zaman asimi          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ "\u001B[0m");
+                System.out.println(Color+ BackGround +saniye + ".0000 sn  proses zaman asimi    (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+" sn )"+ "\u001B[0m");
+            else
+                System.out.println(Color+ BackGround +saniye + ".0000 sn  proses zaman asimi   (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+" sn )"+ "\u001B[0m");
+        if(saniye>=10)
+            if (id<10)
+                System.out.println(Color+ BackGround +saniye + ".0000 sn  proses zaman asimi   (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+" sn )"+ "\u001B[0m");
+            else
+                System.out.println(Color+ BackGround +saniye + ".0000 sn  proses zaman asimi  (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+" sn )"+ "\u001B[0m");
+
     }
     public void askida(int saniye,int kalanSure){
         this.olumAdimi=0;
-        System.out.println(Color +saniye + ".0000 sn  proses askida         (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ "\u001B[0m");
+        if(saniye<10)
+            if(id<10)
+
+                System.out.println(Color+ BackGround +saniye + ".0000 sn proses askida          (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
+            else
+                System.out.println(Color+ BackGround +saniye + ".0000 sn proses askida           (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
+        if(saniye>=10)
+            if (id<10)
+                System.out.println(Color+ BackGround +saniye + ".0000 sn proses askida         (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
+            else
+                System.out.println(Color+ BackGround +saniye + ".0000 sn proses askida        (id:"+id+"  oncelik:"+oncelik+"  kalan sure:"+kalanSure+ " sn )"+"\u001B[0m");
+
     }
 }
